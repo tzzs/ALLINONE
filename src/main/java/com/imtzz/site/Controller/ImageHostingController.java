20575c5321c0100fe6c5fdc4384a6d655b989a66
TZZ <1789803837@qq.com>
2020-12-03 01:00:20 +0800

    增加图片上传接口

--- src/main/java/com/imtzz/site/Controller/ImageHostingController.java
+++ src/main/java/com/imtzz/site/Controller/ImageHostingController.java
@@ -1,26 +1,57 @@
 package com.imtzz.site.Controller;
 
+import com.alibaba.fastjson.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
 
-@Controller
+@RestController
 @RequestMapping("/imageHosting")
 public class ImageHostingController {
-    private final  static Logger LOGGER = LoggerFactory.getLogger(ImageHostingController.class);
+    private final static Logger LOGGER = LoggerFactory.getLogger(ImageHostingController.class);
+
+    @Value("${ImageHosting.uploadDir}")
+    private String uploadDir;
 
     @RequestMapping("/upload")
-    @ResponseBody
-    public String upload(@RequestParam(value = "file")CommonsMultipartFile file) throws RuntimeException {
-        if (file.isEmpty()){
+    public JSONObject upload(@RequestParam(value = "file") CommonsMultipartFile file) throws RuntimeException {
+        JSONObject jsonObject = new JSONObject();
 
+        if (file.isEmpty()) {
+            jsonObject.put("res", "图片为空");
+            return jsonObject;
         }
-        return "Hello world!";
+
+        String fileName = file.getOriginalFilename();
+        LOGGER.info("图片文件名：" + fileName);
+        assert fileName != null;
+        String suffixName = fileName.substring(fileName.lastIndexOf("."));
+        LOGGER.info("图片后缀名：" + suffixName);
+
+        String filePath = uploadDir;
+        File dest = new File(filePath + fileName);
+
+        if (!dest.getParentFile().exists()) {
+            dest.getParentFile().mkdirs();
+        }
+
+        try {
+            file.transferTo(dest);
+            LOGGER.info("上传文件路径：" + filePath + fileName);
+            jsonObject.put("res", fileName);
+            return jsonObject;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return jsonObject;
     }
 }
